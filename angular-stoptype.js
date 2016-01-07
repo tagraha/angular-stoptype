7c8a8bcbad96516ffab2164e49acbbbcbb5142af
Tirta Nugraha <betotrr@gmail.com>
2016-01-08 04:02:38 +0700

    exclude tab key, shift key and enter key
    Preventing directive to fire up when tab, enter, or shift key pressed

--- angular-stoptype.js
+++ angular-stoptype.js
@@ -31,8 +31,16 @@
         
         var doneTypingDelay = new Delay();
         
-        elements.bind("keyup", function() {          
-          doneTypingDelay._init();
+        elements.bind("keyup", function(event) {          
+          var keyCode = event.which || event.keyCode;
+          if (keyCode === 13 || keyCode === 9 || keyCode === 16) {
+            //16 for tab key
+            //13 for enter key
+            //9 for shift key
+            event.preventDefault();
+          } else{
+            doneTypingDelay._init();
+          }
         });
       }
     };
